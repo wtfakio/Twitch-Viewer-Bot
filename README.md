77422c93dc4f5bb683adcbf80c7d6312cfc170e8
Akio <98855346+wtfakio@users.noreply.github.com>
2023-05-18 12:44:02 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+![219774639-6c550f44-ed0c-4dcd-ba09-f87836dcbaa3 (1)](https://media.discordapp.net/attachments/719985605466914856/1108796859633377351/Untitled_Project.jpg)
+ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
+[Discord](https://discord.gg/CjcntDDM9z)ㅤ[Github](https://github.com/wtfakio)
+
+
+### No need for a proxy. ⚠️
+### Please leave a star for the project. This project is free and open source. ⭐ You can leave a star from the top right. ⭐
+
+
+This software allows you to send viewers to your Twitch live stream without the need for a proxy. The way it works is through Selenium, which enters proxy sites, types the channel name, and then reduces the stream quality to 160p. This allows you to connect to the live stream as a viewer through a proxy without having to pay for one.
+
+- The working logic is simple. [kichi779.online](https://www.kichi779.online/) is a proxy website that connects to the website you want with proxies, and this code automates the process with Selenium.
+
+⚠️ it is recommended to use this software on another computer (not the one you are streaming from) or on a computer with a large amount of RAM because it opens a lot of hidden tabs in the background and can send as many viewers as you want to the stream
+
+# Preview 💬
+https://user-images.githubusercontent.com/92625816/223770986-79017b4f-dc14-44b9-bd88-0a70aaeda2ee.mp4
+
+# 📥 Installation & Usage
+
+### - If you have problems with the installation, come to my [discord server](https://discord.gg/CjcntDDM9z)
+
+- Python must be installed
+- If your Python is not installed, download the latest version (3.11) of python and make sure to click on the 'ADD TO PATH' during installation.
+
+- ### Install the chromedriver version compatible with the installed version of Google Chrome on your computer. ⚠️
+
+  https://sites.google.com/chromium.org/driver/?pli=1
+  
+```sh
+git clone https://github.com/wtfakio/Twitch-Viewer-Bot akioviewerbot
+
+cd akioviewerbot
+
+pip install -r requirements.txt (Or you can click on install.bat file)
+```
+## Usage ⚙️
+
+- **After completing the installation process, make sure that you have successfully installed the chromedriver.**
+
+
+- **Then run the run.bat file or double click the main.py file.**
+
+
+# Usage recommendations 📖
+
+- This system opens multiple background sites, so make sure you have plenty of RAM or perform this operation on an unused computer. (It could be on a powerful virtual server.)
+
+- If the audience delivery takes too long and sends only a few viewers, close the client and reopen it, then repeat the same steps. (Proxy sites can sometimes slow down.)
+
+# Stars ⭐
+- Send more viewers and monetize ads / 50 Star ⭐ ❌
+- You can send viewers over the internet with just one click without downloading / 100 Star ⭐⭐⭐ ❌
+
+# [🛒 Shop](N/A at this time!)
+  ㅤㅤ  ㅤ
+ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤWebsite: N/A at this time
+
+
+# ⚠️Legal⚠️
+
+The software designed to perform website security testing. The author is not responsible for any illegal use of these programs. This is 100% educational, please do not misuse this tool. 
+
+If there is a legal problem, please contact my e-mail address. 
+
+akiohayashi22@gmail.com
---
+++ main.py
@@ -0,0 +1,108 @@
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from colorama import Fore
+from pystyle import Center, Colors, Colorate
+import os
+import time
+
+
+def check_for_updates():
+    try:
+        r = requests.get("https://raw.githubusercontent.com/Kichi779/Twitch-Viewer-Bot/main/version.txt")
+        remote_version = r.content.decode('utf-8').strip()
+        local_version = open('version.txt', 'r').read().strip()
+        if remote_version != local_version:
+            print("A new version is available. Please download the latest version from GitHub.")
+            time.sleep(3)
+            return False
+        return True
+    except:
+        return True
+
+
+def main():
+    if not check_for_updates():
+        return
+
+    os.system(f"title Kichi779 - Twitch Viewer Bot @kichi#0779 ")
+
+    print(Colorate.Vertical(Colors.green_to_cyan, Center.XCenter("""
+           
+                           ▄████████         ▄█   ▄█▄       ▄█        ▄██████▄  
+                          ███    ███        ███ ▄███▀      ███       ███    ███ 
+                          ███    ███        ███▐██▀        ███▌      ███    ███ 
+                          ███    ███       ▄█████▀         ███▌      ███    ███ 
+                        ▀███████████      ▀▀█████▄         ███▌      ███    ███ 
+                          ███    ███        ███▐██▄        ███       ███    ███ 
+                          ███    ███        ███ ▀███▄      ███       ███    ███ 
+                          ███    █▀         ███   ▀█▀      █▀         ▀██████▀  
+                                            ▀                                   
+            Improvements can be made to the code. If you're getting an error, visit my discord.
+                                        Discord discord.gg/CjcntDDM9z    
+                                        Github  github.com/wtfakio          
+                       
+                      
+                      """)))
+
+    # Chrome location
+    chrome_path = 'C:\Program Files\Google\Chrome\Application\chrome.exe'
+    # The driver that has the same version as your currently installed Google chrome must be installed.
+    driver_path = 'chromedriver.exe'
+
+    twitch_username = input(Colorate.Vertical(Colors.green_to_blue, ("Enter your channel name (e.g papaevan69): ")))
+    proxy_count = int(input(Colorate.Vertical(Colors.cyan_to_blue, ("How many proxy sites do you want to open? (Viewer to send)"))))
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    chrome_options.add_argument('--disable-logging')
+    chrome_options.add_argument('--log-level=3')
+    chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument("--mute-audio")
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.binary_location = chrome_path
+    driver = webdriver.Chrome(executable_path=driver_path, chrome_options=chrome_options)
+
+    driver.get('https://www.blockaway.net')
+
+    text_box = driver.find_element(By.ID, 'url')
+    text_box.send_keys(f'www.twitch.tv/{twitch_username}')
+    text_box.send_keys(Keys.RETURN)
+
+    for i in range(proxy_count):
+        driver.execute_script("window.open('https://www.blockaway.net')")
+        driver.switch_to.window(driver.window_handles[-1])
+        text_box = driver.find_element(By.ID, 'url')
+        text_box.send_keys(f'www.twitch.tv/{twitch_username}')
+        text_box.send_keys(Keys.RETURN)
+
+    input(Colorate.Vertical(Colors.red_to_blue,("Viewers have all been sent. You can press enter to withdraw the views and the program will close..")))
+    driver.quit()
+
+
+if __name__ == '__main__':
+    main()
+
+# ==========================================
+# Copyright 2023 WTFAKIO
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==========================================
